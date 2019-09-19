6aa993abcdf2ec04c676213c9083291164381810
brett <brettwin@gmail.com>
2019-09-19 13:32:38 -0700

    Updated Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+#create a blog models
+#add blog app to settings
+    # Title
+    # pub date
+    # body
+    # image
+
+#create migration
+#migrate
+#add to admin
+
+
+class Blog(models.Model):
+    Title = models.CharField("Title", max_length=255)
+    Publication_Date = models.DateTimeField()
+    Body = models.TextField()
+    image = models.ImageField(upload_to='images/')
